ac3987ad824d66f81207734f18be7eaa3b7881f7
Chen Li <yichen.li0830@gmail.com>
2017-12-25 21:28:54 -0800

    Update testCredentials file

--- src/testCredentials.js
+++ src/testCredentials.js
@@ -6,7 +6,12 @@ async function fetchBalance(exchangeId){
   let exchange = new ccxt[exchangeId] (ccxt.extend({enableRateLimit: true}, credentials[exchangeId]))
 
   let balance = await exchange.fetchBalance()
-  console.log(`${exchangeId} credential is working: BTC balance is ${balance['free']['BTC']}`)
+  if (balance['free'] && !isNaN(balance['free']['BTC'])) {
+    console.log(`${exchangeId} credential is working: BTC balance is ${balance['free']['BTC']}`)
+  } else {
+    console.log(exchangeId, balance)
+//    console.log(`${exchangeId} credential abnormal: `, Object.keys(balance))
+  }
 }
 
 async function fetchAll(){
@@ -18,8 +23,8 @@ async function fetchAll(){
 
 (async () => {
   try {
-//    await fetchAll()
-    await fetchBalance('quadrigacx')
+    await fetchAll()
+//    await fetchBalance('quadrigacx')
   } catch (e) {
     api.handleError(e)
   }

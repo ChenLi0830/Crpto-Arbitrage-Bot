1821132ae4f503f258bd31ddf8dc91658144fbfd
Chen Li <yichen.li0830@gmail.com>
2017-12-28 16:59:45 -0800

    Implement checkExchangeThatHas method - used to check which exchanges have a specific method/property and update credential file

---
+++ src/utils/checkExchangeThatHas.js
@@ -0,0 +1,24 @@
+const ccxt = require('ccxt')
+const log = require('ololog').configure({locate: false})
+require('ansicolor').nice
+const json2csv = require('json2csv');
+const fs = require('fs')
+const credentials = require('../../credentials')
+const util = require('util')
+
+async function checkExchangeThatHas(methodName){
+  ccxt.exchanges.forEach(exchangeId=>{
+    let exchange = new ccxt[exchangeId]()
+    if (exchange[methodName]) {
+      log(`${exchangeId} has ${methodName}`.green)
+      /** update credential file */
+      if (credentials[exchangeId]) credentials[exchangeId][`_${methodName}`] = true
+//      log(credentials)
+      fs.writeFileSync('credentials.js', 'module.exports = ' + util.inspect(credentials), 'utf-8')
+    }
+  })
+
+  log(ccxt.exchanges.join(' '))
+}
+
+checkExchangeThatHas('fetchDepositAddress')

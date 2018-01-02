d6afdf4ff085f881d75b9949d8de7a26edb7aef7
Chen Li <yichen.li0830@gmail.com>
2018-01-01 23:55:40 -0800

    Implement timeWalk - part 1

--- src/fetchKlines.js
+++ src/fetchKlines.js
@@ -18,12 +18,12 @@ const KLINE_FILE = './savedData/klines.js'
 
 //-----------------------------------------------------------------------------
 
-function getAverage(ohlcv, window, lineLength, ohlcvIndex){
+function getAverage(ohlcv, window, ohlcvIndex){
   let endIdx = ohlcv.length - 1
   let startIdx = ohlcv.length - window
   let result = []
 
-  for (let shift=0; shift<lineLength; shift++) {
+  for (let shift=0; shift<ohlcv.length - Math.max(...windows); shift++) {
     let value = 0
     for (let i=startIdx-shift; i<=endIdx-shift; i++) {
       value += (ohlcv[i][ohlcvIndex] / window)
@@ -35,25 +35,25 @@ function getAverage(ohlcv, window, lineLength, ohlcvIndex){
 }
 
 function checkValueCriteria(klines, volumeLine, index) {
-
-  let isVolumeIncrease = volumeLine[index] > volumeLine[index-1]
   let isFastKlineLarger = (klines[windows[0]][index] > klines[windows[1]][index]) && (klines[windows[0]][index] > klines[windows[2]][index])
   let isMiddleKlineLarger = klines[windows[1]][index] > klines[windows[2]][index]
 
-  return isVolumeIncrease && isFastKlineLarger
+  return isFastKlineLarger
 }
 
 function checkBuyingCriteria(klines, volumeLine) {
+  let isVolumeIncrease = volumeLine[lineLength-1] > volumeLine[lineLength-2]
+
   let currentPoint = lineLength-1
   let prevPoint = lineLength-2
 
-  let nowMatchCriteria = checkValueCriteria(klines, volumeLine, currentPoint)
-  let prevMatchCriteria = checkValueCriteria(klines, volumeLine, prevPoint)
+  let nowValueMatchCriteria = checkValueCriteria(klines, volumeLine, currentPoint)
+  let prevValueMatchCriteria = checkValueCriteria(klines, volumeLine, prevPoint)
 
-  log(`nowMatchCriteria`, nowMatchCriteria)
-  log(`prevMatchCriteria`, prevMatchCriteria)
+//  log(`nowMatchCriteria`, nowMatchCriteria)
+//  log(`prevMatchCriteria`, prevMatchCriteria)
 
-  return nowMatchCriteria && !prevMatchCriteria
+  return nowValueMatchCriteria && !prevValueMatchCriteria && isVolumeIncrease
 }
 
 function rateCurrency(klines, volumeLine) {
@@ -65,20 +65,14 @@ function rateCurrency(klines, volumeLine) {
 function rateAndSort(extractedInfoList) {
   let buyingPool = []
 
-  for (let symbol of Object.keys(extractedInfoList)) {
-    let extractedInfo = extractedInfoList[symbol]
-
-    if (symbol === 'ETH/BTC') {
-      console.log('extractedInfo', extractedInfo)
-    }
-
+  for (let extractedInfo of extractedInfoList) {
     const {klines, volumeLine} = extractedInfo
     let matchBuyingCriteria = checkBuyingCriteria(klines, volumeLine)
 
     if (matchBuyingCriteria) {
       let rate = rateCurrency(klines, volumeLine)
 
-      buyingPool.push({symbol, ...extractedInfo, rate})
+      buyingPool.push({...extractedInfo, rate})
     }
   }
 
@@ -94,6 +88,36 @@ function printLine(lineData){
   log.yellow('\n' + chart, bitcoinRate, '\n')
 }
 
+function timeWalk(extractedInfoList){
+  let shift = 0
+  while (shift + lineLength < 500) {
+    let newExtractedInfoList = extractedInfoList.map(extractedInfo => {
+      let newKlines = {}
+      Object.keys(extractedInfo.klines).forEach(key => {
+        log('extractedInfo.klines[key].length', extractedInfo.klines[key].length)
+        newKlines[key] = extractedInfo.klines[key].slice(shift, shift + lineLength)
+//        log(`newKlines.length`, newKlines.length)
+      })
+      return {
+        ...extractedInfo,
+        klines: newKlines,
+        volumeLine: extractedInfo.volumeLine.slice(shift, shift + lineLength),
+      }
+    })
+
+//    log('newExtractedInfoList', JSON.stringify(newExtractedInfoList))
+    fs.writeFileSync(`${KLINE_FILE}-${shift}`, 'module.exports = ' + JSON.stringify(newExtractedInfoList), 'utf-8')
+
+    let sortedPool = rateAndSort(newExtractedInfoList)
+    if (sortedPool.length > 0) {
+      console.log('sortedPool', sortedPool)
+    }
+    console.log(`${(500-shift - lineLength) * 15} mins ago from 11:00pm`)
+    log(`shift ${shift}`)
+    shift ++
+  }
+}
+
 (async function main () {
   while (true) { // keep fetching
 //    await api.sleep(intervalInMillesec * 0.6)
@@ -101,8 +125,7 @@ function printLine(lineData){
       let exchange = new ccxt.binance()
       await exchange.loadMarkets()
 
-
-      let extractedInfoList = {}
+      let extractedInfoList = []
       for (let symbol of exchange.symbols) {
         let klines = {}
         let volumeLine = []
@@ -114,27 +137,26 @@ function printLine(lineData){
           const lineData = ohlcv.slice(-lineLength).map(x => x[ohlcvIndex]) // closing price
 //          console.log('lineData', lineData)
 //          printLine(lineData)
-          volumeLine = ohlcv.slice(-lineLength).map(x => x[5])
 
           for (let window of windows) {
-            klines[window] = getAverage(ohlcv, window, lineLength, ohlcvIndex)
-            //          printLine(klines[window])
+            klines[window] = getAverage(ohlcv, window, ohlcvIndex)
+//            printLine(klines[window])
           }
 
-          extractedInfoList[symbol] = {
+          extractedInfoList.push({
+            symbol,
             klines,
             volumeLine
-          }
-
+          })
         }
       }
 
 //      const extractedInfoList = require('../savedData/klines')
-      let sortedPool = rateAndSort(extractedInfoList)
-      console.log('sortedPool', sortedPool)
+////      let sortedPool = rateAndSort(extractedInfoList)
+////      log(`sortedPool`, sortedPool)
+//      timeWalk(extractedInfoList)
 
       fs.writeFileSync(KLINE_FILE, 'module.exports = ' + JSON.stringify(extractedInfoList), 'utf-8')
-
 
     } catch (e) {
       console.error(e)

8c8936d589278bf3ba65c551f8ea0a6fdc865dd9
Chen Li <yichen.li0830@gmail.com>
2018-01-01 23:04:29 -0800

    Implementing rateAndSort based on the Kline and volume information

--- src/fetchKlines.js
+++ src/fetchKlines.js
@@ -6,32 +6,85 @@ const asTable = require('as-table')
 const log = require('ololog').configure({locate: false})
 const api = require('./api')
 const fs = require('fs')
+const _ = require('lodash')
 require('ansicolor').nice;
 
 let interval = '15m'
 let intervalInMillesec = 15 * 60 * 1000
-let windows = [7, 25, 99] // maximum = 500 - lineLength
-let lineLength = 100
+let windows = [7, 25, 99] // 必须从小到大，maximum = 500 - lineLength
+let lineLength = 50
 const ohlcvIndex = 4 // [ timestamp, open, high, low, close, volume ]
 const KLINE_FILE = './savedData/klines.js'
+
 //-----------------------------------------------------------------------------
 
 function getAverage(ohlcv, window, lineLength, ohlcvIndex){
   let endIdx = ohlcv.length - 1
-  let startIdx = ohlcv.length - window + 1
+  let startIdx = ohlcv.length - window
   let result = []
 
   for (let shift=0; shift<lineLength; shift++) {
-
     let value = 0
     for (let i=startIdx-shift; i<=endIdx-shift; i++) {
-      value += ohlcv[i][ohlcvIndex] / window
+      value += (ohlcv[i][ohlcvIndex] / window)
     }
-
     result.unshift(value)
   }
 
   return result
+}
+
+function checkValueCriteria(klines, volumeLine, index) {
+
+  let isVolumeIncrease = volumeLine[index] > volumeLine[index-1]
+  let isFastKlineLarger = (klines[windows[0]][index] > klines[windows[1]][index]) && (klines[windows[0]][index] > klines[windows[2]][index])
+  let isMiddleKlineLarger = klines[windows[1]][index] > klines[windows[2]][index]
+
+  return isVolumeIncrease && isFastKlineLarger
+}
+
+function checkBuyingCriteria(klines, volumeLine) {
+  let currentPoint = lineLength-1
+  let prevPoint = lineLength-2
+
+  let nowMatchCriteria = checkValueCriteria(klines, volumeLine, currentPoint)
+  let prevMatchCriteria = checkValueCriteria(klines, volumeLine, prevPoint)
+
+  log(`nowMatchCriteria`, nowMatchCriteria)
+  log(`prevMatchCriteria`, prevMatchCriteria)
+
+  return nowMatchCriteria && !prevMatchCriteria
+}
+
+function rateCurrency(klines, volumeLine) {
+  let deriveK = (klines[windows[0]][lineLength - 1] - klines[windows[0]][lineLength - 2]) / klines[windows[0]][lineLength - 2]
+  let deriveVolume = (volumeLine[lineLength - 1] - volumeLine[lineLength - 2]) / volumeLine[lineLength - 2]
+  return deriveK * deriveVolume
+}
+
+function rateAndSort(extractedInfoList) {
+  let buyingPool = []
+
+  for (let symbol of Object.keys(extractedInfoList)) {
+    let extractedInfo = extractedInfoList[symbol]
+
+    if (symbol === 'ETH/BTC') {
+      console.log('extractedInfo', extractedInfo)
+    }
+
+    const {klines, volumeLine} = extractedInfo
+    let matchBuyingCriteria = checkBuyingCriteria(klines, volumeLine)
+
+    if (matchBuyingCriteria) {
+      let rate = rateCurrency(klines, volumeLine)
+
+      buyingPool.push({symbol, ...extractedInfo, rate})
+    }
+  }
+
+  let sortedPool = _.sortBy(buyingPool, item => item.rate)
+
+  return sortedPool
 }
 
 function printLine(lineData){
@@ -48,7 +101,8 @@ function printLine(lineData){
       let exchange = new ccxt.binance()
       await exchange.loadMarkets()
 
-      let saveData = {}
+
+      let extractedInfoList = {}
       for (let symbol of exchange.symbols) {
         let klines = {}
         let volumeLine = []
@@ -58,7 +112,8 @@ function printLine(lineData){
           const ohlcv = await new ccxt.binance().fetchOHLCV(symbol, interval)
 
           const lineData = ohlcv.slice(-lineLength).map(x => x[ohlcvIndex]) // closing price
-          //        printLine(lineData)
+//          console.log('lineData', lineData)
+//          printLine(lineData)
           volumeLine = ohlcv.slice(-lineLength).map(x => x[5])
 
           for (let window of windows) {
@@ -66,13 +121,20 @@ function printLine(lineData){
             //          printLine(klines[window])
           }
 
-          saveData[symbol] = {
+          extractedInfoList[symbol] = {
             klines,
             volumeLine
           }
+
         }
-        fs.writeFileSync(KLINE_FILE, 'module.exports = ' + JSON.stringify(saveData), 'utf-8')
       }
+
+//      const extractedInfoList = require('../savedData/klines')
+      let sortedPool = rateAndSort(extractedInfoList)
+      console.log('sortedPool', sortedPool)
+
+      fs.writeFileSync(KLINE_FILE, 'module.exports = ' + JSON.stringify(extractedInfoList), 'utf-8')
+
 
     } catch (e) {
       console.error(e)
